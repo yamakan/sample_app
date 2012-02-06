3d9884ee6f9b53efe058c3c1cd9e04c1c1eae38a
Kan Yammamoto <kanbasketball@gmail.com>
2012-02-06 14:53:38 +0900

    User signup complete

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,11 @@ require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
 require 'rails/all'
+
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+
+
 # require "rails/test_unit/railtie"
 
 Bundler.require(:default, Rails.env) if defined?(Bundler)
@@ -35,6 +40,10 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+     # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+    
 if Rails.env.test?
       initializer :after => :initialize_dependency_mechanism do
       ActiveSupport::Dependencies.mechanism = :load
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,9 +5,24 @@ describe UsersController do
 
   describe "GET 'show'" do
     
+    
+    before(:each) do 
+      @user = Factory(:user)
+    end
+    
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+    
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+    
     it "should have the right title" do
       get :show, :id => @user
-      response.should have_selector("tilte", :content => puser.name)
+      response.should have_selector("title", :content => @user.name)
     end
   
     it "should include the user's name" do
@@ -23,19 +38,6 @@ describe UsersController do
   
   describe "GET 'new'" do
     
-    before(:each) do 
-      @user = Factory(:user)
-    end
-    
-    it "should be successful" do
-      get :show, :id => @user
-      response.should be_success
-    end
-    
-    it "should find the right user" do
-      get :show, :id => @user
-      assigns(:user).should == @user
-    end    
     
     it "should be successful" do
       get 'new'
@@ -47,4 +49,57 @@ describe UsersController do
       response.should have_selector("title", :content => "Sign up")
     end
   end
+  
+  describe "POST 'create'" do
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+        
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up") 
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "success" do
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com", :password => "foobar", 
+        :password_confirmation => "foobar" }
+      end
+      
+      it  "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end    
+          
+      it  "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+    end        
+  end
 end
+      
+       
+        
+        
+        
