45ed05c4e83288a7edf699803cc352e199c61767
Kan Yammamoto <kanbasketball@gmail.com>
2012-02-09 22:18:42 +0900

    Done with sign in

--- config/application.rb
+++ config/application.rb
@@ -44,10 +44,10 @@ module SampleApp
      # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
     
-if Rails.env.test?
+    if Rails.env.test?
       initializer :after => :initialize_dependency_mechanism do
-      ActiveSupport::Dependencies.mechanism = :load
+        ActiveSupport::Dependencies.mechanism = :load
+      end
     end
   end
- end
 end
